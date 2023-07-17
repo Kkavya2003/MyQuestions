c51d6bbbd8cc55dbb43867b85991219fe75e2161
Kkavya <kkavyagoyal2707@gmail.com>
2023-07-17 23:00:15 +0530

    initial commit

---
+++ src/AddDigits.java
@@ -0,0 +1,22 @@
+/* Question: Given an integer num, repeatedly add all its digits until
+the resulthas only one digit, and return it.
+ */
+/*Solution:*/
+/* class Solution {
+    public int addDigits(int num) {
+    int sum= 0;
+        while(num>0){
+            int temp = num%10;
+            sum+=temp;
+            num/=10;
+        }
+        if(sum/10==0){
+            return sum;
+        }
+        else{
+            return addDigits(sum);
+        }
+    }
+}
+ */
+

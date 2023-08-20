8eeec445747df7577853419fed028cebb1cecb16
Kkavya2003 <139260798+Kkavya2003@users.noreply.github.com>
2023-08-20 23:54:37 +0530

    Update XOROperationinanArray.java

--- src/XOROperationinanArray.java
+++ src/XOROperationinanArray.java
@@ -3,10 +3,7 @@ You are given an integer n and an integer start.
 Define an array nums where nums[i] = start + 2 * i (0-indexed) and n == nums.length.
 Return the bitwise XOR of all elements of nums.
  */
-
 /*Solution*/
-
-/*
 class Solution {
     public int xorOperation(int n, int start) {
         int num = 0,sol=0;
@@ -17,4 +14,3 @@ class Solution {
     return sol;
     }
 }
- */

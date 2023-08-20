efd75408860880cc9ccb706c5d2a6e6c23307581
Kkavya2003 <139260798+Kkavya2003@users.noreply.github.com>
2023-08-20 23:48:36 +0530

    Update SingNum.java

--- src/SingNum.java
+++ src/SingNum.java
@@ -2,9 +2,8 @@
  Find that single one.
 You must implement a solution with a linear runtime complexity and use only constant extra space.
  */
-
 /*Solution:*/
-/* class Solution {
+class Solution {
     public int singleNumber(int[] nums) {
         int len = nums.length;
         Arrays.sort(nums);
@@ -16,5 +15,3 @@ You must implement a solution with a linear runtime complexity and use only cons
     return nums[len - 1];
     }
 }
-
- */

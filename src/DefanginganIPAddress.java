f568ee70079c71cd22ad0bd2a06a760a445a23cc
Kkavya2003 <139260798+Kkavya2003@users.noreply.github.com>
2023-08-20 23:37:56 +0530

    Update DefanginganIPAddress.java

--- src/DefanginganIPAddress.java
+++ src/DefanginganIPAddress.java
@@ -5,7 +5,6 @@ A defanged IP address replaces every period "." with "[.]".
 
 /*Solution*/
 
-/*
 class Solution {
     public String defangIPaddr(String address) {
         String str="";
@@ -13,4 +12,4 @@ class Solution {
     return str;
     }
 }
- */
+ 

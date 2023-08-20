7338c686ec9ef2370a7721af8a202f79f0a2955f
Kkavya2003 <139260798+Kkavya2003@users.noreply.github.com>
2023-08-20 23:45:23 +0530

    Update PlusOne.java

--- src/PlusOne.java
+++ src/PlusOne.java
@@ -7,7 +7,7 @@ Increment the large integer by one and return the resulting array of digits.
 
 /*Solution*/
 
-/*
+
 class Solution {
     public int[] plusOne(int[] digits) {
         int len = digits.length;
@@ -26,4 +26,4 @@ class Solution {
     return arr;
     }
 }
- */
+ 

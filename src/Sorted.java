11de8e04deb6ebfaed679a7a096525305650f7da
Kkavya2003 <139260798+Kkavya2003@users.noreply.github.com>
2023-08-20 23:50:04 +0530

    Update Sorted.java

--- src/Sorted.java
+++ src/Sorted.java
@@ -1,7 +1,8 @@
 /* Question : Given an integer array nums sorted in non-decreasing order,
  return an array of the squares of each number sorted in non-decreasing order.
  */
-/* class Solution {
+/*Solution*/ 
+class Solution {
     public int[] sortedSquares(int[] nums) {
         int len = nums.length;
         int [] sqr = new int [len];

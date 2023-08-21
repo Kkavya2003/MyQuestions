542de52ef06a539a717369551447d804bf3fa6bf
Kkavya <kkavyagoyal2707@gmail.com>
2023-08-21 23:09:04 +0530

    initial commit

--- src/ToLowerCase.java
+++ src/ToLowerCase.java
@@ -3,7 +3,7 @@ Given a string s, return the string after
 replacing every uppercase letter with the same lowercase letter.
  */
 /*Solution*/
-class Solution {
+class ToLowerCase{
     public String toLowerCase(String s) {
         String str="";
         str = s.toLowerCase();
